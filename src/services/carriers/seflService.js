814d4dc54c292f9626342b373527c16bdd08e30f
conshipai <thomas.ehler@conship.ai>
2025-08-27 21:38:01 -0500

    Create seflService.js

---
+++ src/services/carriers/seflService.js
@@ -0,0 +1,11 @@
+// Placeholder SEFL service
+const getSEFLRates = async (config, shipmentData) => {
+  console.log('SEFL rates not implemented yet');
+  
+  // Return empty rates array for now
+  return [];
+  
+  // TODO: Implement actual SEFL API integration
+};
+
+export default getSEFLRates;
