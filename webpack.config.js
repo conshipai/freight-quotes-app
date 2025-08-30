d885e5ba26ad9ee799d2eb5d94fdad48fe5b0ef3
conshipai <thomas.ehler@conship.ai>
2025-08-29 20:48:44 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,84 @@
-new ModuleFederationPlugin({
-  name: 'quotes',
-  filename: 'remoteEntry.js',
-  exposes: {
-    './App': './src/App',
-    './Widget': './src/Widget',
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/bootstrap.js',
+  output: {
+    publicPath: 'auto',
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
- shared: {
-  react: {
-    singleton: true,
-    eager: true,
-    requiredVersion: require('./package.json').dependencies.react,
+  resolve: {
+    extensions: ['.jsx', '.js', '.json'],
   },
-  'react-dom': {
-    singleton: true,
-    eager: true,
-    requiredVersion: require('./package.json').dependencies['react-dom'],
+  devServer: {
+    port: 3002,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
   },
-  'react-router-dom': { 
-    singleton: true,
-    eager: true,
-    requiredVersion: require('./package.json').dependencies['react-router-dom'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
   },
-},
-}),
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify({
+        REACT_APP_API_URL: process.env.REACT_APP_API_URL || 'https://api.gcc.conship.ai/api',
+        NODE_ENV: process.env.NODE_ENV || 'development',
+      })
+    }),
+    new ModuleFederationPlugin({
+      name: 'quotes',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/App',
+        './Widget': './src/Widget',
+      },
+      shared: {
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: require('./package.json').dependencies.react,
+        },
+        'react-dom': {
+          singleton: true,
+          eager: true,
+          requiredVersion: require('./package.json').dependencies['react-dom'],
+        },
+        'react-router-dom': { 
+          singleton: true,
+          eager: true,
+          requiredVersion: require('./package.json').dependencies['react-router-dom'],
+        },
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+};
