3aba003e5b39830c14499c93f1cd87fc39f1b65d
conshipai <thomas.ehler@conship.ai>
2025-08-29 20:45:24 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,11 @@
 // src/App.jsx
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { QuoteProvider } from './contexts/QuoteContext';
 import QuoteRouter from './components/QuoteRouter';
 
-function App({ shellContext, basename }) {
-  // If running standalone (not in shell), provide our own router
-  const isStandalone = !window.shellContext && !shellContext;
-  
-  if (isStandalone) {
-    // Running standalone - provide our own BrowserRouter
-    const { BrowserRouter } = require('react-router-dom');
-    return (
-      <BrowserRouter basename={basename}>
-        <AuthProvider>
-          <ThemeProvider>
-            <QuoteProvider>
-              <QuoteRouter />
-            </QuoteProvider>
-          </ThemeProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    );
-  }
-  
-  // Running in shell - shell provides the router
+function App() {
   return (
     <AuthProvider>
       <ThemeProvider>
