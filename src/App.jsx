dc836884f3284aac9c082cebbaaef8fa33d0932f
conshipai <thomas.ehler@conship.ai>
2025-08-29 20:39:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { QuoteProvider } from './contexts/QuoteContext';
 import QuoteRouter from './components/QuoteRouter';
 
-function App() {
+function App({ shellContext, basename }) {
+  // If running standalone (not in shell), provide our own router
+  const isStandalone = !window.shellContext && !shellContext;
+  
+  if (isStandalone) {
+    // Running standalone - provide our own BrowserRouter
+    const { BrowserRouter } = require('react-router-dom');
+    return (
+      <BrowserRouter basename={basename}>
+        <AuthProvider>
+          <ThemeProvider>
+            <QuoteProvider>
+              <QuoteRouter />
+            </QuoteProvider>
+          </ThemeProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    );
+  }
+  
+  // Running in shell - shell provides the router
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <ThemeProvider>
-          <QuoteProvider>
-            <QuoteRouter />
-          </QuoteProvider>
-        </ThemeProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <ThemeProvider>
+        <QuoteProvider>
+          <QuoteRouter />
+        </QuoteProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
 
 export default App;
-
